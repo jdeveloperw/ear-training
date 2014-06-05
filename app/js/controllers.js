17e8a7d15d387650c104334b7ed22b59b903676c
JD White <jdeveloperw@gmail.com>
2014-06-04 18:48:52 -0700

    Add play button

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,19 +4,23 @@
 
 angular.module('myApp.controllers', [])
   .controller('MainController', ['$scope', function($scope) {
-    console.log('here');
+  }])
+  .controller('PlayerController', ['$scope', function($scope) {
 
-    MIDI.loadPlugin({
-      soundfontUrl: "bower_components/MIDI.js/soundfont/",
-      instrument: "acoustic_grand_piano",
-      callback: function() {
-        var delay = 0; // play one note every quarter second
-        var note = 50; // the MIDI note
-        var velocity = 127; // how hard the note hits
-        // play the note
-        MIDI.setVolume(0, 127);
-        MIDI.noteOn(0, note, velocity, delay);
-        MIDI.noteOff(0, note, delay + 0.75);
-      }
-    });
-  }]);
+    this.play = function() {
+      MIDI.loadPlugin({
+        soundfontUrl: "bower_components/MIDI.js/soundfont/",
+        instrument: "acoustic_grand_piano",
+        callback: function() {
+          var delay = 0; // play one note every quarter second
+          var note = 50; // the MIDI note
+          var velocity = 127; // how hard the note hits
+          // play the note
+          MIDI.setVolume(0, 127);
+          MIDI.noteOn(0, note, velocity, delay);
+          MIDI.noteOff(0, note, delay + 0.75);
+        }
+      });
+    }
+  }])
+  ;
