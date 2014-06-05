d1238e2c9c010dae0bdedf19797ff18e416d76dd
JD White <jdeveloperw@gmail.com>
2014-06-04 18:40:32 -0700

    Playing 1 note

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MainController', ['$scope', function($scope) {
+    console.log('here');
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+    MIDI.loadPlugin({
+      soundfontUrl: "bower_components/MIDI.js/soundfont/",
+      instrument: "acoustic_grand_piano",
+      callback: function() {
+        var delay = 0; // play one note every quarter second
+        var note = 50; // the MIDI note
+        var velocity = 127; // how hard the note hits
+        // play the note
+        MIDI.setVolume(0, 127);
+        MIDI.noteOn(0, note, velocity, delay);
+        MIDI.noteOff(0, note, delay + 0.75);
+      }
+    });
   }]);
